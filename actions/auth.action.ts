22a12e0dc0c8871bf56fced3ec1f47c52a29be95
Diwanshu <saidiwanshu1880@gmail.com>
2024-06-13 22:32:14 +0530

    fix: Authentication and Changed Schema for emailVerified

--- actions/auth.action.ts
+++
@@ -1,8 +0,0 @@
-"use server"
-
-import { signIn } from "@/auth"
-
-export const SocialLoginAction = async (provider:"google" | "github") => {
-       
-        await signIn(provider, { callbackUrl: "/" })
-}
--- components/auth/card.tsx
+++ components/auth/card.tsx
@@ -1,25 +1,27 @@
-"use client"
+"use client";
 
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
-import { Button } from "../ui/button"
-import { SocialLoginAction } from "@/actions/auth.action"
-import { FaGithub, FaGoogle } from "react-icons/fa"
-import { useSearchParams } from "next/navigation"
-import { signIn } from "@/auth"
-import { DEFAULT_LOGIN_REDIRECT } from "@/routes"
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "../ui/button";
+
+import { FaGithub, FaGoogle } from "react-icons/fa";
+import { useSearchParams } from "next/navigation";
+import { signIn } from "next-auth/react";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { FormEvent } from "react";
 
 const CardComponent = () => {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get("callbackUrl");
- 
-  const handleOauth = (provider: string) => {
+
+  const handleOauth = (e: FormEvent<HTMLButtonElement>, provider: string) => {
+    e.preventDefault();
     signIn(provider, {
       callbackUrl: callbackUrl || DEFAULT_LOGIN_REDIRECT,
     });
@@ -28,24 +30,35 @@ const CardComponent = () => {
     <Card className="w-[350px]">
       <CardHeader>
         <CardTitle className="text-center">Login</CardTitle>
-        <CardDescription className="text-center">Increase Your Study Productivity 76%.</CardDescription>
+        <CardDescription className="text-center">
+          Increase Your Study Productivity 76%.
+        </CardDescription>
       </CardHeader>
       <CardContent>
         <form>
           <div className="grid w-full items-center gap-4">
-                <Button onClick={()=> handleOauth("google")} className="gap-2" ><FaGoogle />SignIn with Google </Button>
-                <Button onClick={()=> handleOauth("github")} variant={"outline"} className="gap-2"><FaGithub />SignIn with Github </Button>
+            <Button onClick={(e) => handleOauth(e, "google")} className="gap-2">
+              <FaGoogle />
+              SignIn with Google{" "}
+            </Button>
+            <Button
+              onClick={(e) => handleOauth(e, "github")}
+              variant={"outline"}
+              className="gap-2"
+            >
+              <FaGithub />
+              SignIn with Github{" "}
+            </Button>
           </div>
         </form>
-        <CardFooter >
+        <CardFooter>
           <p className="text-foreground/30 text-xs text-center mt-4">
-                 By signing in, you agree to our Terms of Service and Privacy Policy
+            By signing in, you agree to our Terms of Service and Privacy Policy
           </p>
         </CardFooter>
       </CardContent>
-    
     </Card>
-  )
-}
+  );
+};
 
-export default CardComponent
+export default CardComponent;
